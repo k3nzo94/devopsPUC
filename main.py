ebe010a4d163d2f80e64dc1fbe6b0ea0986a687a
k3nzoSK <eduardok3nzo@gmail.com>
2026-03-24 18:10:07 -0300

    2

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def read_root():
 
 @app.get('/funcaoteste')
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
